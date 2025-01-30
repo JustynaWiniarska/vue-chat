f234b92d2804eb28a6a84f6f8e6b5dae1a2412db
Justyna <justyna.field@gmail.com>
2025-01-30 11:09:16 +0100

    Develops MessageBox styling

--- src/components/ConversationArea.vue
+++ src/components/ConversationArea.vue
@@ -1,3 +1,26 @@
+<script>
+import MessageBox from './MessageBox.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MessageBox
+  }
+}
+</script>
+
 <template>
-  <div>Conversation area</div>
+  <div class="conversation-container">
+    <h1>Conversation area</h1>
+
+    <MessageBox />
+
+  </div>
 </template>
+
+<style lang="css" scoped>
+.conversation-container {
+  border: 2px solid gray;
+  height: 400px
+}
+</style>
---
+++ src/components/MessageBox.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: 'MessageBox',
+}
+</script>
+
+<template>
+  <div class="message-container">
+    <div class="thumbnail"></div>
+    <div class="message-bubble">message  </div>
+  </div>
+</template>
+
+<style scoped>
+.message-container {
+  display: flex;
+  align-items: flex-end;
+  padding: 20px;
+}
+
+.thumbnail {
+  width: 48px;
+  height: 48px;
+  background-color: teal;
+  border-radius: 50%;
+  margin-right: 12px;
+}
+
+.message-bubble {
+  border: 2px solid darkgray;
+  border-radius: 8px;
+  padding: 8px;
+  width: fit-content;
+  max-width: 60%;
+  text-align: left;
+}
+</style>
