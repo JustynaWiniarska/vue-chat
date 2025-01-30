9fbc9fbcbb3fdb350b088a2f84146674f64f6735
Justyna <justyna.field@gmail.com>
2025-01-30 20:15:46 +0100

    Initiates vuex

--- src/main.js
+++ src/main.js
@@ -1,4 +1,39 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createStore } from 'vuex'
 
-createApp(App).mount('#app')
+const store = createStore({
+  state() {
+    return {
+      messages: []
+    }
+  },
+  mutations: {
+    setMessages(state, payload) {
+      state.users = payload
+    }
+  },
+  actions: {
+    async fetchMessages({commit}) {
+      try {
+        const response = await fetch('http://localhost:3001/messages')
+        const messages = await response.json()
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch messages')
+        }
+
+        commit('setMessages', messages)
+      }
+      catch (e) {
+        console.error('Error fetching messages: ', e)
+      }
+    }
+  }
+})
+
+const app = createApp(App)
+
+app.use(store)
+
+app.mount('#app')
